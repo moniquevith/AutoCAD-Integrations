d754abf748962673e003dc70b4589166aba2f553
Monique Vith <monique.vith@arkance.world>
2025-12-02 16:32:36 +1100

    added some file paths to iterate

---
+++ updateTable.py
@@ -0,0 +1,44 @@
+from pyautocad import Autocad, APoint
+
+# automatically connect to open acad
+acad = Autocad(create_if_not_exists=True)
+doc = acad.ActiveDocument
+# acad = Autocad()
+
+files = [
+    r"C:\Users\Monique.Vith\OneDrive - Groupe Monnoyeur\Documents\AutoCad\drawings\test1.dwg",
+    r"C:\Users\Monique.Vith\OneDrive - Groupe Monnoyeur\Documents\AutoCad\drawings\test2.dwg",
+    r"C:\Users\Monique.Vith\OneDrive - Groupe Monnoyeur\Documents\AutoCad\drawings\test3.dwg",
+    r"C:\Users\Monique.Vith\OneDrive - Groupe Monnoyeur\Documents\AutoCad\drawings\test4.dwg"
+]
+
+target_table = None
+search_text = "Test Table".lower()
+
+for f in files:
+    doc = acad.app.Documents.Open(f)
+
+    for obj in acad.iter_objects():
+        if obj.ObjectName == "AcDbTable":
+            cols = obj.Columns
+            for col in range(cols):
+                cell_text = obj.GetText(0, col)
+                if cell_text and search_text in cell_text.lower():
+                    target_table = obj
+                    print("Found matching table")
+                    break
+            if target_table:
+                target_table.SetText(1, 1, "Hello") # add text
+                target_table.SetText(2, 1, "World")
+                target_table.SetText(2, 2, "World")
+
+                target_table.SetTextHeight(1, 40) # change font size to 40
+
+                target_table.SetAlignment(1, 5) # middle centre align (5th selection)
+                target_table.Update()
+    doc.SaveAs(f)
+    
+if not target_table:
+    print("No table with 'test table' in header row found.")
+else:
+    print("Table identified successfully.")
